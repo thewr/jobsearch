0b5bb201c106796b19dbff04708fe43c5db24782
thewr <36010296+thewr@users.noreply.github.com>
2019-04-05 14:34:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -96,7 +96,7 @@ $(function(){
    // $("add_item_button").show();
   });
   
-$('table#item-list tbody tr').click(function() {
+$('table#item-list tbody').click(function() {
       $(this).toggleClass('selected');
 });
   
