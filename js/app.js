9039a41a5e65939fec4791da82f07629cfd18ccf
thewr <36010296+thewr@users.noreply.github.com>
2019-04-05 16:40:49 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ $(function(){
   $(".content").hide();
   $("#menutag").html("Menu");
   $("#new_item").click(function(){
-    $('.leftmenu').css({width: '33%'})
+    $('.leftmenu').css({width:'calc(10%+350px)'})
     $("#options").hide();
      $(".content").hide().fadeIn(1000); 
    $( ".feedback" ).toggleClass('blur-me');
