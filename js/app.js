494afe3d5d9dea5b1b6a44fefeefcec590efe3e6
thewr <36010296+thewr@users.noreply.github.com>
2019-05-03 13:59:15 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -46,8 +46,8 @@ function myFunc(evt)
 {
 	const id = evt.target.parentElement.getAttribute('data-id');
 	console.log(id);
-	if(evt.target && evt.target.nodeName == "selected") {
-            console.log(evt.target.id + " was clicked");
+	if(evt.target && evt.target.nodeName == 'li' && evt.taget.className = 'selected') {
+            console.log(evt.target.className + " was clicked");
         }
 	if(id == null){
 		return;
