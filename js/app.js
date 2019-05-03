e9f9c4424f4d3b9dff0f5ac8bb847d90eebd5274
thewr <36010296+thewr@users.noreply.github.com>
2019-05-03 14:18:56 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function refresh() {
     //}, 100);
 	//clear data
 	var count = itemList.count;
-	while(count>-1){
+	while(count){
 		console.log('removing ... ' + itemList.count);
 		count -= 1;
 		itemList.removeChild(itemList.childNodes[0]);
