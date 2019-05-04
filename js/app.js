6ef620c2dc8fb78f769591e4dd1f9772987aba84
thewr <36010296+thewr@users.noreply.github.com>
2019-05-04 15:24:27 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -56,6 +56,7 @@ function todaysDate()
     return today;
 }
 
+/*
 function clearForm()
 {
       form.name.value = '';
@@ -65,6 +66,7 @@ function clearForm()
       form.wordsD.value = '';
       form.subject.value = '';
 }
+*/
 
 function myFunc(evt)
 {
