36805c099445b2284df11929941160be6e0ddafd
thewr <36010296+thewr@users.noreply.github.com>
2019-05-16 18:54:16 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -248,7 +248,7 @@ function renderDB(doc){
  // var item_date = doc.data().date;
   //var myJSON = JSON.stringify(todaysDate());
   var t = document.createTextNode(doc.data().date);
-  t.style.cssText = "color: black; font-weight: bold;"; //border: 1px solid black";
+  t.newCell.setAttribute("STYLE","color:red'");
   itemList.appendChild(t);
 
   li.appendChild(label_name);
