1ca2f84296d792bce432397af902beda0b697ae7
thewr <36010296+thewr@users.noreply.github.com>
2019-05-29 12:36:56 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -447,7 +447,7 @@ $('#item_cancel').click(function(){
 
 	
 	$('#item-list').on('click','li',function() {
-	$(this).addClass('selected');//toggleClass('selected');//.siblings().removeClass('selected');
+	$(this).toggleClass('selected').siblings().removeClass('selected');
 		if($(this).hasClass('selected')){
 			//$(this).append($div);
 			$("#edit_item").show();
@@ -457,7 +457,6 @@ $('#item_cancel').click(function(){
 		} else {
 			//$(this).css("background-color", "");
 			// $div.remove();
-			$(this).removeClass('selected');
       			$("#edit_item").hide();
 			$("#delete_item").hide();
 
