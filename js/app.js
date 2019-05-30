802dc0a9284ecfda57b971b1e8cc7d4ef174c15e
thewr <36010296+thewr@users.noreply.github.com>
2019-05-30 15:24:46 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -129,7 +129,7 @@ function clearForm()
 function myFunc(evt)
 {
 	const id = evt.target.parentElement.getAttribute('data-id');
-	evt.target.classList.add('selected');
+	evt.target.parent(li).classList.add('selected');
 	
 	if(evt.target.nodeName == 'SPAN'){// && evt.taget.className = 'selected') {
             console.log(id + " was clicked");	
