2b89659cda3ab0dcb5da7341a2702ad2bc1d15ce
thewr <36010296+thewr@users.noreply.github.com>
2020-02-17 12:43:35 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -70,9 +70,8 @@ function renderDB(doc){
 	 // var item_date = doc.data().date;	
 	  //var myJSON = JSON.stringify(todaysDate());	
 	let date_label = document.createElement('label');	
-	//label.style = "color:black; text-align: center; display: block; font-weight: bold"; // apply your style	
+	label.style = "color:black; text-align: center; display: block; font-weight: bold"; // apply your style	
 	  date_label.textContent = doc.data().date;	
-	//label.appendChild(document.createTextNode(doc.data().date));	
 	itemList.appendChild(date_label);	
   }	
 	
