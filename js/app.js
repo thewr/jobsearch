0ede95680f8608cde3ff7588116ceecfde6ecf4f
thewr <36010296+thewr@users.noreply.github.com>
2020-02-19 14:09:54 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -183,7 +183,7 @@ function renderDB(doc){
 
 
 // real-time listener
-db.collection('applications').orderBy('date').onSnapshot(snapshot => {
+db.collection('applications').orderBy('date','desc').orderBy('name').onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
     changes.forEach(change => {
 	    if(change.type == 'added'){
